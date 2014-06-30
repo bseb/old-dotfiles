ac2e2e2a845c5ac1b76a730280d2283e48d22a7d
ben <ben@teroknor.(none)>
2014-06-30 19:47:24 -0400

    fix syntax changes

--- installdots.sh
+++ installdots.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-rm -r ~/.vimrc ~/.screenrc ~/.xmodmap
+# Remove existing dotfiles if present
+rm -r ~/.vimrc ~/.screenrc ~/.xmodmap ~/.bashrc
+rm -r ~/.config/awesome/rc.lua
+#Make directory for awesomewm rc.lua
+mkdir -p ~/.config/awesome
+#Link files in dotfiles to where they go
+ln -s ~/dotfiles/bashrc ~/.bashrc 
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/screenrc ~/.screenrc
 ln -s ~/dotfiles/xmodmap ~/.xmodmap
+ln -s ~/dotfiles/rc.lua ~/.config/awesome/rc.lua
+
