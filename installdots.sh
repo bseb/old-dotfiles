451cbd52bc83278722096876893e653e51a6e287
Ben Sebastian <ben.sebastian@icloud.com>
2014-08-05 22:46:15 +0000

    Added support for zsh, removed xmodmap as its pretty useless

--- installdots.sh
+++ installdots.sh
@@ -6,21 +6,22 @@ if [ -e /etc/xdg/awesome/rc.lua ]; then
 	rm -r ~/.config/awesome/rc.lua
 	ln -s ~/dotfiles/rc.lua ~/.config/awesome/rc.lua
 fi
-#Assume that if we are in runlevel 5 that we have x running
-if [ "$(runlevel | awk '{ print $2 }')" -eq 5 ]; then
-	rm -r ~/.xmodmap
-	ln -s ~/dotfiles/xmodmap ~/.xmodmap
-fi
 # Remove existing dotfiles if present
 rm -r ~/.vimrc ~/.screenrc ~/.bashrc ~/.tmux.conf
 #Link files in dotfiles to where they go
 ln -s ~/dotfiles/bashrc ~/.bashrc 
 ln -s ~/dotfiles/vimrc ~/.vimrc
+#Multiplexers a-gogo 
 if [ -e /usr/bin/screen ];then
 	ln -s ~/dotfiles/screenrc ~/.screenrc
 fi
 if [ -e /usr/bin/tmux ];then
 	ln -s ~/dotfiles/tmux ~/.tmux.conf
+fi
+#zsh setup, Thanks a lot Nathan
+if [ -e /usr/bin/zsh ];then 
+	ln -s ~/dotfiles/oh-my-zsh ~/.oh-my-zsh
+	ln -s ~/dotfiles/zshrc ~/.zshrc
 fi
 #Install Pathogen.vim to manage vim plugins and download the plugins I use
 #pathogen.vim
